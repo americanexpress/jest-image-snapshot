cf8d0f59aef181d6e8c2116cf8aeb69cb99a22ce
avlyalin <lyalin.alexey@gmail.com>
2020-05-27 16:36:38 +0300

    test(index): added mock for "supports-color", added new tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -17,10 +17,12 @@ const fs = require('fs');
 const path = require('path');
 
 describe('toMatchImageSnapshot', () => {
-  function setupMock(diffImageToSnapshotResult) {
+  function setupMock(diffImageToSnapshotResult, mockSupportsColor = true) {
     jest.doMock('../src/diff-snapshot', () => ({
       runDiffImageToSnapshot: jest.fn(() => diffImageToSnapshotResult),
     }));
+
+    jest.mock('supports-color', () => mockSupportsColor);
 
     const mockFs = Object.assign({}, fs, {
       existsSync: jest.fn(),
@@ -146,12 +148,46 @@ describe('toMatchImageSnapshot', () => {
       diffRatio: 0.4,
       diffPixelCount: 600,
     };
+    const mockSupportsColor = false;
+
+    setupMock(mockDiffResult, mockSupportsColor);
+    const { toMatchImageSnapshot } = require('../src/index');
+    expect.extend({ toMatchImageSnapshot });
+
+    expect(() => expect('pretendthisisanimagebuffer').toMatchImageSnapshot({ noColors: false }))
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('should not style error message if colors not supported ', () => {
+    const mockDiffResult = {
+      pass: false,
+      diffOutputPath: 'path/to/result.png',
+      diffRatio: 0.4,
+      diffPixelCount: 600,
+    };
+    const mockSupportsColor = false;
+
+    setupMock(mockDiffResult, mockSupportsColor);
+    const { toMatchImageSnapshot } = require('../src/index');
+    expect.extend({ toMatchImageSnapshot });
+
+    expect(() => expect('pretendthisisanimagebuffer').toMatchImageSnapshot())
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('should style error message if colors supported ', () => {
+    const mockDiffResult = {
+      pass: false,
+      diffOutputPath: 'path/to/result.png',
+      diffRatio: 0.4,
+      diffPixelCount: 600,
+    };
 
     setupMock(mockDiffResult);
     const { toMatchImageSnapshot } = require('../src/index');
     expect.extend({ toMatchImageSnapshot });
 
-    expect(() => expect('pretendthisisanimagebuffer').toMatchImageSnapshot({ noColors: false }))
+    expect(() => expect('pretendthisisanimagebuffer').toMatchImageSnapshot())
       .toThrowErrorMatchingSnapshot();
   });
 
